9782b2b01d61ffd17a6595eb008b8901121a2de4
arthur <design.mgn@gmail.com>
2022-08-07 20:12:10 +0300

    working with sts-tokens

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/minio/minio-go/v7"
+	cr "github.com/minio/minio-go/v7/pkg/credentials"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"net/url"
 	"os"
 )
 
@@ -41,14 +40,23 @@ var participantPolicy = `{
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Sid": "GetObjectsInFolder",
+      "Effect" : "Allow",
+      "Action" : [
+        "s3:ListBucket"
+      ],
+		"Resource" : [
+		  "arn:aws:s3:::photostudio"
+		],
+		"Condition":{"StringLike":{"s3:prefix":"%[1]s"}}
+    },
+    {
       "Sid": "GetListObjectsBuckets",
       "Effect" : "Allow",
       "Action" : [
         "s3:GetObject"
       ],
-        "Resource" : [
-          "arn:aws:s3:::photostudio/%s"
-        ]
+	  "Resource": ["arn:aws:s3:::photostudio/%[1]s/*"]
     }
   ]
 }`
@@ -59,69 +67,55 @@ func main() {
 	secretKey := viper.GetString("s3.secretKey")
 	log.Info("host: ", host)
 
-	// Initialize a session in us-west-2 that the SDK will use to load
-	// credentials from the shared credentials file ~/.aws/credentials.
-	sess, err := session.NewSession(&aws.Config{
-		Credentials: credentials.NewStaticCredentials(accessKey, secretKey, ""),
-		Endpoint:    aws.String(host),
-		Region:      aws.String("minio"),
-	})
+	var stsOpts cr.STSAssumeRoleOptions
+	stsOpts.AccessKey = accessKey
+	stsOpts.SecretKey = secretKey
 
-	if err != nil {
-		fmt.Println("NewSession Error", err)
-		return
-	}
-	//participantPolicy, err := getPolicy("policy/participant.json")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
 	id := "Sibay"
-	// Create a STS client
-	svc := sts.New(sess)
-	role, err := svc.AssumeRole(&sts.AssumeRoleInput{
-		DurationSeconds: aws.Int64(3600),
-		Policy:          aws.String(fmt.Sprintf(participantPolicy, id)),
-		RoleArn:         aws.String("arn:x:ignored:by:minio:"), // arn:partition:service:region:account-id:resource-id
-		RoleSessionName: aws.String("ignored-by-minio"),
-		SourceIdentity:  aws.String("photostudio"),
-	})
-	if err != nil {
-		log.Fatal("AssumeRole: ", err)
-	}
-	fmt.Println("Role: ", *role)
-	//
-	//mainS3svc := s3.New(sess)
-	//if _, err := mainS3svc.PutObject(&s3.PutObjectInput{
-	//	ACL:    aws.String("public-read"),
-	//	Body:   nil,
-	//	Bucket: aws.String("photostudio"),
-	//	Key:    aws.String("photostudio/" + id + "/"),
-	//}); err != nil {
-	//	log.Fatal("PutObject: s", err)
-	//}
-	userSess, err := session.NewSession(&aws.Config{
-		Credentials: credentials.NewStaticCredentials(*role.Credentials.AccessKeyId, *role.Credentials.SecretAccessKey, *role.Credentials.SessionToken),
-		Endpoint:    aws.String(host),
-		Region:      aws.String("minio"),
-	})
-	if err != nil {
-		log.Fatal("NewSession: ", err)
-	}
-	userS3svc := s3.New(userSess)
-	getObject, err := userS3svc.ListObjectsV2(&s3.ListObjectsV2Input{
-		Bucket: aws.String("photostudio"),
-	})
-	//buckets, err := userS3svc.ListBuckets(&s3.ListBucketsInput{})
-	if err != nil {
-		log.Error("GetObject: ", err)
-	}
-	fmt.Println("getObject: ", getObject)
+	policy := fmt.Sprintf(participantPolicy, id)
+	stsOpts.Policy = policy
+	stsOpts.DurationSeconds = 300
 
-	accessKeyInfo, err := svc.GetAccessKeyInfo(&sts.GetAccessKeyInfoInput{
-		AccessKeyId: role.Credentials.AccessKeyId,
-	})
+	li, err := cr.NewSTSAssumeRole(host, stsOpts)
 	if err != nil {
-		log.Error("GetAccessKeyInfo: ", err)
+		log.Fatalf("Error initializing STS Identity: %v", err)
 	}
-	fmt.Println(accessKeyInfo)
+
+	stsEndpointURL, err := url.Parse(host)
+	if err != nil {
+		log.Fatalf("Error parsing sts endpoint: %v", err)
+	}
+
+	opts := &minio.Options{
+		Creds:  li,
+		Secure: stsEndpointURL.Scheme == "https",
+	}
+
+	if _, err := li.Get(); err != nil {
+		log.Fatalf("Error retrieving STS credentials: %v", err)
+	}
+
+	//fmt.Println("Only displaying credentials:")
+	//fmt.Println("AccessKeyID:", v.AccessKeyID)
+	//fmt.Println("SecretAccessKey:", v.SecretAccessKey)
+	//fmt.Println("SessionToken:", v.SessionToken)
+
+	// Use generated credentials to authenticate with MinIO server
+	minioClient, err := minio.New(stsEndpointURL.Host, opts)
+	if err != nil {
+		log.Fatalf("Error initializing client: %v", err)
+	}
+
+	bucket := "photostudio"
+	fmt.Printf("\nCalling list objects on bucket named `%s` with temp creds:\n===\n", bucket)
+	objCh := minioClient.ListObjects(context.Background(), bucket, minio.ListObjectsOptions{
+		Prefix:    id,
+		Recursive: true,
+	})
+	for obj := range objCh {
+		if obj.Err != nil {
+			log.Fatalf("Listing error: %v", obj.Err)
+		}
+		fmt.Printf("Key: %s\nSize: %d\nLast Modified: %s\n===\n", obj.Key, obj.Size, obj.LastModified)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,44 +3,38 @@ module photostudio
 go 1.18
 
 require (
-	github.com/aws/aws-sdk-go v1.44.66
-	github.com/aws/aws-sdk-go-v2/config v1.15.14
-	github.com/aws/aws-sdk-go-v2/credentials v1.12.9
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.1
+	github.com/minio/minio-go/v7 v7.0.34
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/viper v1.12.0
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.16.7 // indirect
-	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.3 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.8 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.14 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.8 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.15 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.5 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.3 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.9 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.8 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.8 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.11.12 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.16.9 // indirect
-	github.com/aws/smithy-go v1.12.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/rs/xid v1.4.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/ini.v1 v1.66.6 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
